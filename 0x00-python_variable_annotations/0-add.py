16b79a6faf158eb72482e4fc1f1184708e410816
veromejia <veromejia.q@gmail.com>
2020-10-19 15:59:51 -0700

    task 0-2

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/ev python3
+""" function add"""
+
+
+def add(a: float, b: float) -> float:
+    """function that add two floats and return a float"""
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" function floor"""
+
+
+def floor(n: float)->int:
+    """takes and return a float"""
+    return int(n)
