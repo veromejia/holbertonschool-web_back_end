e2424d77f925a0e425aba260cc78205a9abc0535
veromejia <veromejia.q@gmail.com>
2020-10-20 22:06:40 -0700

    pep8

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """wait ramdom function"""
     time_delay = random.random() * max_delay
     await asyncio.sleep(time_delay)
