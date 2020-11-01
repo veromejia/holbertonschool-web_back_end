f01d4a57ad97e8b72d1e8786d84917c0b3f88013
veromejia <veromejia.q@gmail.com>
2020-11-01 15:18:08 -0800

    Create logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -8,12 +8,27 @@ import re
 import os
 
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "ip")
+
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Return the log message obfuscated"""
     return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
                   lambda x: x.group(1) + "=" + redaction
                   if x.group(1) in fields else x.group(0), message)
+
+
+def get_logger() -> logging.Logger:
+    """ Return a logger object """
+    log = logging.getLogger("user_data")
+    log.setLevel(logging.INFO)
+    log.propagate = False
+    sh = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    sh.setFormatter(formatter)
+    log.addHandler(sh)
+    return log
 
 
 class RedactingFormatter(logging.Formatter):
@@ -24,10 +39,13 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """initalization"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """method to filter values in incoming log records
+        using filter_datum"""
         return filter_datum(self.fields, self.REDACTION,
                             super(RedactingFormatter, self).format(record),
                             self.SEPARATOR)
