aeccfa77bdbfcbceae4e06384617d6ace6dcae66
veromejia <veromejia.q@gmail.com>
2020-11-05 14:50:33 -0800

    pycodestyle correction

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -8,16 +8,12 @@ from typing import List, TypeVar
 
 class Auth:
     """Auth Class that handle public methods"""
-        
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if path requires authentication"""
-        
-    
+
     def authorization_header(self, request=None) -> str:
         """Check if request is authorized"""
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Return current user  """
-
-
-        
