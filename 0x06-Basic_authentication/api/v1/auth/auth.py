16e50f172974a5f7e59483b84898c804c7d024a2
veromejia <veromejia.q@gmail.com>
2020-11-05 14:46:11 -0800

    auth class

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Define Auth class """
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth Class that handle public methods"""
+        
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if path requires authentication"""
+        
+    
+    def authorization_header(self, request=None) -> str:
+        """Check if request is authorized"""
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Return current user  """
+
+
+        
