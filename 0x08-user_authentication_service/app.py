dabde06e64153ab3f6eaf011de42c4326d55b6d7
veromejia <veromejia.q@gmail.com>
2020-11-22 19:26:37 -0800

    app file

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -40,5 +40,18 @@ def login():
     return response
 
 
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """ DELETE /sessions
+    If the user does not exist, respond with a 403 HTTP status"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
