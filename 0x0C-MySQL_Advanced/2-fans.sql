965b042ff13663823e6d644ef9a5957fa255e294
veromejia <veromejia.q@gmail.com>
2020-12-03 20:23:13 -0800

    Best band ever

---
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+-- Select the bands table from the holberton database
+-- ranks country origins of bands, ordered by the number of (non-unique) fans
+SELECT origin,
+       SUM(fans) nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC
